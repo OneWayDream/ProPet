0006aeac9a5457ee089ada8eaa123e693552b7b1
Erik Nasibullin <nasibullinei@internet.ru>
2022-04-17 17:16:35 +0300

    Add search page

--- ProPet Service/ProPet Frontend/src/App.js
+++ ProPet Service/ProPet Frontend/src/App.js
@@ -4,13 +4,15 @@ import MainPage from './components/pages/mainPage';
 import ProfilePage from './components/pages/profilePage';
 import SignInPage from './components/pages/signInPage/';
 import SignUpPage from './components/pages/signUpPage';
+import SearchPage from './components/pages/searchPage';
 
 function App() {
   return (
     // <MainPage/>
     // <ProfilePage/>
     // <SignInPage/>
-    <SignUpPage/>
+    // <SignUpPage/>
+    <SearchPage/>
   );
 }
 
---
+++ ProPet Service/ProPet Frontend/src/components/pages/searchPage/SearchPage.js
@@ -0,0 +1,39 @@
+import Image from "../../atoms/image";
+import Template from "../template";
+import FilterLogo from "../../../img/filter.png"
+
+const SearchPage = () => {
+
+  const showFilter = () => {
+    console.log("AAA");
+  }
+
+
+  const body = 
+    <div style={{ padding: '0 1vw', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ paddingRight: '1vw' }} >
+        <div style={{ display: 'flex' }}>
+          <div>
+            <input type="text" className="searchInput" placeholder="Кот, собака" />            
+          </div>
+          <div>
+            <button className="submitButton">Найти</button>
+          </div>
+        </div>
+        <div>
+          Results
+        </div>
+      </div>
+      <div className="filter">
+        <div onClick={showFilter}>
+          <Image image={FilterLogo} width='4vw'/>
+        </div>
+        <div style={{ backgroundColor: 'red', }}>
+          test
+        </div>
+      </div>
+    </div>
+  return <Template body={body}/>
+} 
+
+export default SearchPage;
